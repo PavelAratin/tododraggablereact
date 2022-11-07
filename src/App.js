8e4b0d37890869e7f40efdca0c61277c8ba0342c
Pavel <paha_gagarin@mail.ru>
2022-11-07 17:32:21 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import { v4 as uuid } from 'uuid';
+import { randomColor } from 'randomcolor';
+import Draggable from 'react-draggable';
 
 function App() {
+  const [item, setItem] = useState('');
+  const [items, setItems] = useState(
+    JSON.parse(localStorage.getItem('items')) || []
+  );
+
+  useEffect(() => {
+    localStorage.setItem('items', JSON.stringify(items))
+  }, [items])
+
+  const newItemHandler = () => {
+    if (item.trim() !== '') {
+      const newItem = {
+        id: uuid(),
+        title: item,
+        color: randomColor({
+          luminosity: 'light'
+        }),
+        defaultPos: {
+          x: 650,
+          y: -600
+        }
+      }
+      setItems((items) => {
+        return [
+          ...items,
+          newItem
+        ]
+      })
+      setItem('')
+    } else {
+      alert('Enter something')
+      setItem('')
+    }
+  }
+  const deleteNode = (id) => {
+    setItems(items.filter((item) => item.id !== id))
+  }
+  const updatePos = (data, index) => {
+    let newArray = [...items]
+    newArray[index].defaultPos = { x: data.x, y: data.y }
+    setItems(newArray)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="wrapper">
+        <input
+          value={item}
+          onChange={e => setItem(e.target.value)}
+          type='text'
+          placeholder='Enter something'
+        ></input>
+        <button onClick={newItemHandler} className="enter">ENTER</button>
+      </div>
+      {items.map((item, index) => {
+        return (
+          <Draggable
+            key={index}
+            defaultPosition={item.defaultPos}
+            onStop={(_, data) => {
+              updatePos(data, index)
+            }}>
+            <div className="todo__item" style={{ backgroundColor: item.color }}>
+              {`${item.title}`}
+              <button
+                className="delete"
+                onClick={() => deleteNode(item.id)}
+              >X</button>
+            </div>
+          </Draggable>
+        )
+      })}
     </div>
   );
 }
